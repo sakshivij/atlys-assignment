46ac25d4a2446f82f1422f9d3ae3803c6281ea99
Sakshi Vij <sakshivij12@gmail.com>
2024-11-22 23:34:43 +0530

    Adding changes

--- web-scrapping-tool/local-setup/generate_env.sh
+++ web-scrapping-tool/local-setup/generate_env.sh
@@ -2,3 +2,5 @@ HOST="127.0.0.1"
 PORT=8000
 ORIGINS="http://localhost:8080"
 DATABASE_URL=mongodb://localhost:27017
+USEDATABASE="true"
+TOKEN="token"
---
+++ web-scrapping-tool/web_scrapping_tool/utils/authentication.py
@@ -0,0 +1,18 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+
+from ..config import EnvironmentVariables
+
+env = EnvironmentVariables()
+
+STATIC_TOKEN = env.token
+
+bearer_scheme = HTTPBearer()
+
+def verify_token(credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme)):
+    if credentials.credentials != STATIC_TOKEN:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalid or missing token",
+        )
+    return credentials.credentials
